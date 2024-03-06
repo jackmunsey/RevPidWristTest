486638981918f62a1a43dc997456aa0075bdfa07
Jack Munsey <jackeverettmunsey@gmail.com>
2024-03-06 09:00:14 -0800

    did  some updates to make it work needs testing

--- src/main/java/frc/robot/Wrist.java
+++ src/main/java/frc/robot/Wrist.java
@@ -4,33 +4,36 @@
 
 package frc.robot;
 
+import com.revrobotics.AbsoluteEncoder;
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.MotorFeedbackSensor;
 import com.revrobotics.SparkMaxAbsoluteEncoder;
 import com.revrobotics.SparkPIDController;
 import com.revrobotics.CANSparkBase.ControlType;
+import com.revrobotics.SparkAbsoluteEncoder.Type;
 
 import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class Wrist extends SubsystemBase {
   /** Creates a new Wrist. */
+  AbsoluteEncoder encoder;
   SparkPIDController pID;
-    double p = 1;
-    double i = 0;
-    double d = 0;
-    double zone = 0;
-    double fF = .2;
-    double minOutput = 6;
-    double maxOutput = 6;
-    double setpoint = 0;
-
-    // pID = rightMotor.getPIDController();
+  double p = 3;
+  double i = 0;
+  double d = 0;
+  double zone = 0;
+  double fF = 0;
+  double minOutput = 6;
+  double maxOutput = 6;
+  double setPoint = 0;
 
   public Wrist(CANSparkMax rightMotor, CANSparkMax leftmotor) {
 
     pID = rightMotor.getPIDController();
-    pID.setFeedbackDevice(rightMotor.getAbsoluteEncoder(SparkMaxAbsoluteEncoder.Type.kDutyCycle));
+    encoder = rightMotor.getAbsoluteEncoder(Type.kDutyCycle);
+    pID.setFeedbackDevice(encoder);
     
     pID.setP(p);
     pID.setI(i);
@@ -41,12 +44,14 @@ public class Wrist extends SubsystemBase {
   }
 
   public void setPoint(double setPoint) {
-    this.setpoint = setPoint;
+    this.setPoint = setPoint;
   }
 
   @Override
   public void periodic() {
     // This method will be called once per scheduler run
-    pID.setReference(setpoint, ControlType.kPosition, 0);
+    pID.setReference(setPoint, ControlType.kDutyCycle);
+    SmartDashboard.putNumber("setPoint", setPoint);
+    SmartDashboard.putNumber("encoder position", encoder.getPosition());
   }
 }
