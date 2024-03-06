08fa2f878a612b7fc8eeb13e3b3ba7baeea09cf9
Jon Laptop <sotabots@gmail.com>
2024-03-05 20:10:46 -0800

    working on rev pid

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -4,15 +4,46 @@
 
 package frc.robot;
 
+import com.revrobotics.CANSparkBase;
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.SparkPIDController;
+import com.revrobotics.CANSparkLowLevel.MotorType;
+
+import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
+import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 
 public class RobotContainer {
+  private Wrist mWrist;
+  private CommandXboxController mXboxController;
+
   public RobotContainer() {
+    CANSparkMax leftMotor;
+    CANSparkMax rightMotor;
+    
+
+    leftMotor = new CANSparkMax(10, MotorType.kBrushless);
+    rightMotor = new CANSparkMax(11, MotorType.kBrushless);
+
+    rightMotor.setIdleMode(CANSparkBase.IdleMode.kBrake);
+    leftMotor.setIdleMode(CANSparkBase.IdleMode.kBrake);
+    rightMotor.setInverted(true);
+    leftMotor.setInverted(false);
+
+    leftMotor.follow(rightMotor);
+
+    this.mWrist = new Wrist(rightMotor, leftMotor);
+
+    mXboxController = new CommandXboxController(0);
+
     configureBindings();
   }
 
-  private void configureBindings() {}
+  private void configureBindings() {
+    mXboxController.a().onTrue(Commands.run(() -> mWrist.setPoint(0.25), mWrist))
+    .onFalse(Commands.runOnce(() -> mWrist.setPoint(0), mWrist));
+  }
 
   public Command getAutonomousCommand() {
     return Commands.print("No autonomous command configured");
--- src/main/java/frc/robot/Wrist.java
+++ src/main/java/frc/robot/Wrist.java
@@ -4,14 +4,48 @@
 
 package frc.robot;
 
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.MotorFeedbackSensor;
+import com.revrobotics.SparkPIDController;
+import com.revrobotics.CANSparkBase.ControlType;
+
+import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class Wrist extends SubsystemBase {
   /** Creates a new Wrist. */
-  public Wrist() {}
+  SparkPIDController pID;
+    double p = 1;
+    double i = 0;
+    double d = 0;
+    double zone = 0;
+    double fF = .2;
+    double minOutput = 6;
+    double maxOutput = 6;
+    double setpoint = 0;
+
+    // pID = rightMotor.getPIDController();
+
+  public Wrist(CANSparkMax rightMotor, CANSparkMax leftmotor) {
+
+    pID = rightMotor.getPIDController();
+    pID.setFeedbackDevice(rightMotor.getAbsoluteEncoder());
+    
+    pID.setP(p);
+    pID.setI(i);
+    pID.setD(d);
+    pID.setIZone(zone);
+    // pID.setFF(fF);
+    pID.setOutputRange(minOutput, maxOutput);
+  }
+
+  public void setPoint(double setPoint) {
+    this.setpoint = setPoint;
+  }
 
   @Override
   public void periodic() {
     // This method will be called once per scheduler run
+    pID.setReference(setpoint, ControlType.kPosition, 0);
   }
 }
